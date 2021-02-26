1e85b37d67b2cb87830386bd578d8525a44589ea
Luis H <luishenrique.alves@ikasle.egibide.org>
2021-02-26 09:48:28 +0100

    T7P6E1
    APLICACION WEB

---
+++ TEMA 7/PRACTICA 5/apliWEB1/src/java/Clases/Usuario.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author 1gdaw05
+ */
+public class Usuario {
+    private String nombre;
+    private String pass;
+
+    public Usuario(String nombre, String pass) {
+        this.nombre = nombre;
+        this.pass = pass;
+    }
+    
+    
+    
+}
---
+++ TEMA 7/PRACTICA 5/apliWEB1/src/java/controlador.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Clases.Usuario;
+import Excepciones.DatoNoValido;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 1gdaw05
+ */
+@WebServlet(urlPatterns = {"/controlador"})
+public class controlador extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet controlador</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        try{
+            String nombre = request.getParameter("usuario");
+            String pass = request.getParameter("passwd");
+            if(nombre.isEmpty())
+            {
+                throw new DatoNoValido("Usuario Vacio.");
+            }
+            if(pass.isEmpty())
+            {
+                throw new DatoNoValido("Contraseña Vacia.");
+            }
+            nuevoUsuario(nombre, pass);
+            responder("Usuario creado con exito!!!", response);
+        }
+        catch(DatoNoValido e)
+        {
+            responder(e.getMensaje(), response);
+        }
+        
+    }
+    
+    public void responder(String mensaje, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Ejercicio 1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>" + mensaje + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    public void nuevoUsuario(String nombre, String pass) throws ServletException, IOException{
+        Usuario u1 = new Usuario(nombre, pass);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
